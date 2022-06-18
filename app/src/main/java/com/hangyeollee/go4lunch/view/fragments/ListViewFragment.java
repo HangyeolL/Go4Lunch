2da7aa1699059f3588cf64ef4695c0f26223e386
Enarier <gyeol98@gmail.com>
2022-06-18 22:06:32 +0200

    ListViewFragment sorting according to autocomplete logic wrote but not TESTED. Committing to have a save point

--- app/src/main/java/com/hangyeollee/go4lunch/view/fragments/ListViewFragment.java
+++ app/src/main/java/com/hangyeollee/go4lunch/view/fragments/ListViewFragment.java
@@ -12,19 +12,29 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.hangyeollee.go4lunch.R;
 import com.hangyeollee.go4lunch.databinding.FragmentListViewBinding;
+import com.hangyeollee.go4lunch.model.autocompletepojo.MyAutoCompleteData;
+import com.hangyeollee.go4lunch.model.autocompletepojo.Prediction;
 import com.hangyeollee.go4lunch.model.neaerbyserachpojo.MyNearBySearchData;
+import com.hangyeollee.go4lunch.model.neaerbyserachpojo.Result;
 import com.hangyeollee.go4lunch.viewmodel.MapsAndListSharedViewModel;
 import com.hangyeollee.go4lunch.viewmodel.ViewModelFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListViewFragment extends Fragment {
 
     private Location mUserLocation;
+
+    private List<Result> mNearbySearchResultList;
 
     private MapsAndListSharedViewModel mViewModel;
 
@@ -45,31 +55,85 @@ public class ListViewFragment extends Fragment {
             alertBuilder.setMessage("Location is not authorized.\nPlease authorize location permission in settings").create().show();
         }
 
+        //        mViewModel.getLocationLiveData().observe(getViewLifecycleOwner(), new Observer<Location>() {
+        //            @Override
+        //            public void onChanged(Location location) {
+        //                mUserLocation = location;
+        //                mViewModel.fetchNearBySearchData(location.toString());
+        //            }
+        //        });
+
         mViewModel.getLocationLiveData().observe(getViewLifecycleOwner(), new Observer<Location>() {
             @Override
             public void onChanged(Location location) {
                 mUserLocation = location;
-                mViewModel.fetchNearBySearchData(location.toString());
             }
         });
 
-        mViewModel.getNearBySearchLiveData().observe(getViewLifecycleOwner(), new Observer<MyNearBySearchData>() {
-            @Override
-            public void onChanged(MyNearBySearchData myNearBySearchData) {
-                binding.recyclerViewRestaurantList.setAdapter(new ListViewFragmentRecyclerViewAdapter(myNearBySearchData.getResults(), mUserLocation));
-            }
-        });
-
+        nearbySearchRecyclerViewSetup();
+        searchViewSetup();
 
         return binding.getRoot();
     }
-
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         mViewModel.stopLocationRequest();
         binding = null;
+    }
+
+    private void nearbySearchRecyclerViewSetup() {
+        mViewModel.getNearBySearchLiveData().observe(getViewLifecycleOwner(), new Observer<MyNearBySearchData>() {
+            @Override
+            public void onChanged(MyNearBySearchData myNearBySearchData) {
+                mNearbySearchResultList = myNearBySearchData.getResults();
+                binding.recyclerViewRestaurantList.setAdapter(new ListViewFragmentRecyclerViewAdapter(mNearbySearchResultList, mUserLocation));
+            }
+        });
+    }
+
+    private void searchViewSetup() {
+        SearchView searchView = getActivity().findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.length() > 2) {
+//                    mViewModel.fetchAutoCompleteData(newText, mUserLocation.toString());
+                    autoCompleteRecyclerViewSetup();
+                } else {
+                    mViewModel.setAutoCompleteDataLiveDataAsNull();
+                    nearbySearchRecyclerViewSetup();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void autoCompleteRecyclerViewSetup() {
+        mViewModel.getAutoCompleteLiveData().observe(getViewLifecycleOwner(), new Observer<MyAutoCompleteData>() {
+            @Override
+            public void onChanged(MyAutoCompleteData myAutoCompleteData) {
+                List<Prediction> mPredictionList = myAutoCompleteData.getPredictions();
+
+                List<Result> sortedResultList = new ArrayList<>();
+                for (Result result : mNearbySearchResultList) {
+                    for (Prediction prediction : mPredictionList) {
+                        if (prediction.getStructuredFormatting().getMainText().contains(result.getName())) {
+                            sortedResultList.add(result);
+                        }
+                    }
+                }
+                binding.recyclerViewRestaurantList.setAdapter(new ListViewFragmentRecyclerViewAdapter(sortedResultList, mUserLocation));
+            }
+        });
+
+
     }
 
 }
