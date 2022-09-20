8e6db2937cf11a8e0357a0126934aac6840e990a
Enarier <gyeol98@gmail.com>
2022-09-20 18:44:09 +0200

    MapsFragmentViewModel MediatorLiveData coded but not tested

--- app/src/main/java/com/hangyeollee/go4lunch/view/MainHomeActivity/MapsViewFragment/MapsFragmentViewModel.java
+++ app/src/main/java/com/hangyeollee/go4lunch/view/MainHomeActivity/MapsViewFragment/MapsFragmentViewModel.java
@@ -6,15 +6,23 @@ import android.graphics.drawable.Drawable;
 import android.location.Location;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.hangyeollee.go4lunch.model.autocompletepojo.MyAutoCompleteData;
+import com.hangyeollee.go4lunch.model.autocompletepojo.Prediction;
 import com.hangyeollee.go4lunch.model.neaerbyserachpojo.MyNearBySearchData;
+import com.hangyeollee.go4lunch.model.neaerbyserachpojo.Result;
 import com.hangyeollee.go4lunch.repository.AutoCompleteDataRepository;
 import com.hangyeollee.go4lunch.repository.LocationRepository;
 import com.hangyeollee.go4lunch.repository.NearbySearchDataRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nullable;
 
 public class MapsFragmentViewModel extends ViewModel {
 
@@ -22,10 +30,62 @@ public class MapsFragmentViewModel extends ViewModel {
     private NearbySearchDataRepository mNearbySearchDataRepository;
     private AutoCompleteDataRepository mAutoCompleteDataRepository;
 
+    private MediatorLiveData<MapsFragmentViewState> mapsFragmentViewStateMediatorLiveData = new MediatorLiveData<>();
+
+
     public MapsFragmentViewModel(LocationRepository mLocationRepository, NearbySearchDataRepository mNearbySearchDataRepository, AutoCompleteDataRepository mAutoCompleteDataRepository) {
         this.mLocationRepository = mLocationRepository;
         this.mNearbySearchDataRepository = mNearbySearchDataRepository;
         this.mAutoCompleteDataRepository = mAutoCompleteDataRepository;
+
+        LiveData<Location> locationLiveData = mLocationRepository.getLocationLiveData();
+        LiveData<MyNearBySearchData> myNearBySearchDataLiveData = mNearbySearchDataRepository.getNearbySearchLiveData();
+        LiveData<MyAutoCompleteData> myAutoCompleteDataLiveData = mAutoCompleteDataRepository.getAutoCompleteLiveData();
+
+        mapsFragmentViewStateMediatorLiveData.addSource(locationLiveData, location -> {
+            combine(location, myNearBySearchDataLiveData.getValue(), myAutoCompleteDataLiveData.getValue());
+        });
+
+        mapsFragmentViewStateMediatorLiveData.addSource(myNearBySearchDataLiveData, myNearBySearchData -> {
+            combine(locationLiveData.getValue(), myNearBySearchData, myAutoCompleteDataLiveData.getValue());
+        });
+
+        mapsFragmentViewStateMediatorLiveData.addSource(myAutoCompleteDataLiveData, myAutoCompleteData -> {
+            combine(locationLiveData.getValue(), myNearBySearchDataLiveData.getValue(), myAutoCompleteData);
+        });
+
+    }
+
+    private void combine(@Nullable Location location, @Nullable MyNearBySearchData myNearBySearchData, @Nullable MyAutoCompleteData myAutoCompleteData) {
+        if(location == null || myNearBySearchData == null || myAutoCompleteData == null) {
+            return;
+        }
+
+        String userLocation = null;
+
+        if(location != null) {
+            userLocation = location.getLatitude() + "," + location.getLongitude();
+        }
+
+        List<Result> nearBySearchResultList = new ArrayList<>();
+        List<Prediction> myAutoCompletePredictionList = new ArrayList<>();
+
+        for(Result result : myNearBySearchData.getResults()) {
+            nearBySearchResultList.add(result);
+        }
+
+        for(Prediction prediction : myAutoCompleteData.getPredictions()) {
+            myAutoCompletePredictionList.add(prediction);
+        }
+
+        mapsFragmentViewStateMediatorLiveData.setValue(
+                new MapsFragmentViewState(userLocation, nearBySearchResultList, myAutoCompletePredictionList)
+        );
+    }
+
+    // Mediator //
+    public LiveData<MapsFragmentViewState> getMapsFragmentViewStateMediatorLiveData() {
+        return mapsFragmentViewStateMediatorLiveData;
     }
 
     // Location Repo //
@@ -68,4 +128,6 @@ public class MapsFragmentViewModel extends ViewModel {
         drawable.draw(canvas);
         return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
+
+
 }
--- app/src/main/java/com/hangyeollee/go4lunch/view/MainHomeActivity/MapsViewFragment/MapsFragmentViewState.java
+++ app/src/main/java/com/hangyeollee/go4lunch/view/MainHomeActivity/MapsViewFragment/MapsFragmentViewState.java
@@ -1,10 +1,21 @@
 package com.hangyeollee.go4lunch.view.MainHomeActivity.MapsViewFragment;
 
+import com.hangyeollee.go4lunch.model.autocompletepojo.MyAutoCompleteData;
+import com.hangyeollee.go4lunch.model.autocompletepojo.Prediction;
+import com.hangyeollee.go4lunch.model.neaerbyserachpojo.MyNearBySearchData;
+import com.hangyeollee.go4lunch.model.neaerbyserachpojo.Result;
+
+import java.util.List;
+
 public class MapsFragmentViewState {
 
-    private final String markerName;
+    private final String userLocation;
+    private final List<Result> myNearBySearchDataResultList;
+    private final List<Prediction> myAutoCompleteDataPredictionList;
 
-    public MapsFragmentViewState(String markerName) {
-        this.markerName = markerName;
+    public MapsFragmentViewState(String userLocation, List<Result> myNearBySearchDataResultList, List<Prediction> myAutoCompleteDataPredictionList) {
+        this.userLocation = userLocation;
+        this.myNearBySearchDataResultList = myNearBySearchDataResultList;
+        this.myAutoCompleteDataPredictionList = myAutoCompleteDataPredictionList;
     }
 }
